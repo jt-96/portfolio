12bac71575bcfceccb09ef29329bd138dbd44342
jt-96 <jetorrico@outlook.com>
2022-03-23 20:21:14 -0300

    small footer changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/about/About';
 import Profile from './components/profile/Profile';
 import Project from './components/projects/Projects';
 import Contact from './components/contact/Contact';
-// import Footer from './components/footer/Footer';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <Profile />
       <Project />
       <Contact />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,9 +4,9 @@ import './Footer.css';
 function Footer(){
 
     return (
-        <div className="footer">
-            <span>Footer.</span>
-            <span>Footer</span>
+        <div className="footer d-flex flex-column align-items-center">
+            <span>Lorem ipsum dolor sit amet consectetur.</span>
+            <span>Lorem ipsum dolor sit amet.</span>
         </div>
     )
 }
