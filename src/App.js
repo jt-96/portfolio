272ca11b8183c2b32192c06364ad7bde8809e129
jt-96 <jetorrico@outlook.com>
2022-03-23 20:13:18 -0300

    added projects component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/navBar/NavBar';
 import Intro from './components/intro/Intro';
 import About from './components/about/About';
 import Profile from './components/profile/Profile';
+import Project from './components/projects/Projects';
 import Footer from './components/footer/Footer';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Intro />
       <About />
       <Profile />
+      <Project />
       {/* <Footer /> */}
     </div>
   );
