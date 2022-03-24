9c69a8948465db495f65a06cd84be4ecbc98a0c4
jt-96 <jetorrico@outlook.com>
2022-03-24 14:38:51 -0300

    height change and formatting

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Intro.css';
 
-function Intro(){
+function Intro() {
 
     return (
-        <div className='intro text-center align-items-center d-flex flex-column justify-content-center'>
-            <h1 className='title'>Lorem ipsum dolor sit.</h1>
-            <h4 className='subtitle'>Lorem ipsum dolor sit amet consectetur.</h4>
+        <div className='row intro text-center align-items-center justify-content-center'>
+            <div className='col'>
+                <h1 className='title'>Lorem ipsum dolor sit.</h1>
+                <h4 className='subtitle'>Lorem ipsum dolor sit amet consectetur.</h4>
+            </div>
         </div>
     )
 }
