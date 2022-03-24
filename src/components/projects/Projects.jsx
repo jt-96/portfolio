3e6afadbae010cac492b4dc12c253cdc93c0d4e2
jt-96 <jetorrico@outlook.com>
2022-03-24 15:03:56 -0300

    projects formatting

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import './Projects.css';
 
 function Project() {
 
     return (
-        <div className="container projects my-3 py-3" id='projects'>
+        <div className="container projects my-3 py-3" id="projects">
             <h2 className="py-2">Projects</h2>
             <p className="py-2">Here are some of my personal projects done so far.</p>
             <div className="py-3 d-flex flex-row justify-content-around">
                 <div>
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: "18rem" }}>
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
                             <Card.Title>Card Title</Card.Title>
@@ -23,7 +23,7 @@ function Project() {
                     </Card>
                 </div>
                 <div>
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: "18rem" }}>
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
                             <Card.Title>Card Title</Card.Title>
@@ -36,7 +36,7 @@ function Project() {
                     </Card>
                 </div>
                 <div>
-                    <Card style={{ width: '18rem' }}>
+                    <Card style={{ width: "18rem" }}>
                         <Card.Img variant="top" src="holder.js/100px180" />
                         <Card.Body>
                             <Card.Title>Card Title</Card.Title>
