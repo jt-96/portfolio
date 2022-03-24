0b4bd9aa399f1aad991000603968e646758e31ff
jt-96 <jetorrico@outlook.com>
2022-03-24 17:50:07 -0300

    adjusted links anchor

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,8 @@ import './About.css';
 function About(){
 
     return (
-        <div className="container about my-3 py-3" id="about">
+        <div className="container about my-3 py-3">
+            <div className="anchor" id="about"></div>
             <h2 className="py-2">About Me</h2>
             <p className="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit autem quibusdam reprehenderit atque voluptates?</p>
             <p className="col">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi laboriosam, mollitia illum pariatur quaerat et possimus recusandae accusantium expedita ab, odio commodi, dolor ullam reprehenderit!</p>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,7 +5,8 @@ import './Projects.css';
 function Project() {
 
     return (
-        <div className="container projects my-3 py-3" id="projects">
+        <div className="container projects my-3 py-3">
+            <div className="anchor" id="projects"></div>
             <h2 className="py-2">Projects</h2>
             <p className="py-2">Here are some of my personal projects done so far.</p>
             <div className="py-3 d-flex flex-row justify-content-around">
