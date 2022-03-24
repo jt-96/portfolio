fbdf34ce06649a3deb217167415fc19579878e45
jt-96 <jetorrico@outlook.com>
2022-03-24 20:32:46 -0300

    added contact logos and links

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,11 +6,17 @@ function Contact() {
     return (
         <div className="container contact my-3 py-3" id="contact">
             <h2 className="my-2 py-2 text-center">Connect with Me</h2>
-            <div className="my-3 py-3 row justify-content-center text-center">
-                <span className="col-md-2">Icon</span>
-                <span className="col-md-2">Icon</span>
-                <span className="col-md-2">Icon</span>
-                <span className="col-md-2">Icon</span>
+            <div className="flex-row text-center">
+                <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/cv.png`} />
+                <a href="mailto:jetorrico@outlook.com">
+                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/email.png`} />
+                </a>
+                <a href="https://ar.linkedin.com/in/jonathan-torrico" target="_blank">
+                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/linkedin.png`} />
+                </a>
+                <a href="https://github.com/jt-96" target="_blank">
+                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/github.png`} />
+                </a>
             </div>
         </div>
     )
