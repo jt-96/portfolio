4a822f1ef12fbffaebeedc0865645ea4794ca4c9
jt-96 <jetorrico@outlook.com>
2022-03-24 18:14:12 -0300

    changed title color and added font awesome

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,7 +7,7 @@ function Intro() {
         <div className="row g-0 intro text-center align-items-center justify-content-center" id="home">
             <div className="col">
                 <h1 className="title">Welcome to my Portfolio.</h1>
-                <h4 className="subtitle">Jonathan Torrico - Full Stack Developer.</h4>
+                <h4 className="subtitle">Jonathan Torrico - Full Stack Developer</h4>
             </div>
         </div>
     )
