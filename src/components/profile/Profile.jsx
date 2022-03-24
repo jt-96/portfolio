ffd1cfd4a568682cfc4f59002dd360f6083999c2
jt-96 <jetorrico@outlook.com>
2022-03-24 15:02:33 -0300

    profile formatting

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -4,21 +4,21 @@ import './Profile.css';
 function Profile() {
 
     return (
-        <div className="container profile my-3 py-3" id='profile'>
-            <h2 className='py-2'>Profile</h2>
-            <p className='py-2'>Currently I'm based in MERN Stack and this are the tecnologies I used so far.</p>
-            <div className='row py-3 flex-row text-center'>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
-                <span className='col px-3'>Icon</span>
+        <div className="container profile my-3 py-3" id="profile">
+            <h2 className="py-2">Profile</h2>
+            <p className="py-2">Currently I'm based in MERN Stack and this are the tecnologies I used so far.</p>
+            <div className="row py-3 flex-row text-center">
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
+                <span className="col px-3">Icon</span>
             </div>
 
         </div>
