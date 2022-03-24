c8384f07328ccb1f70cd2b3b7a7f70fb3bc43d41
jt-96 <jetorrico@outlook.com>
2022-03-24 19:32:03 -0300

    added logos in profile and removed fontawesome

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -8,18 +8,14 @@ function Profile() {
             <div className="anchor" id="profile"></div>
             <h2 className="py-2">Profile</h2>
             <p className="py-2">Currently I'm based in MERN Stack and this are the tecnologies I used so far.</p>
-            <div className="row py-3 flex-row text-center">
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
-                <span className="col px-3">Icon</span>
+            <div className="py-3 flex-row text-center">
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/html5.png`}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/css3.png`}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/js.png`}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/react.png`}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/node.png`}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/mongodb.png`}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/git.png`}/>
             </div>
 
         </div>
