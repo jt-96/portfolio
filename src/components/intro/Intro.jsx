c1dad19ff6015dcb40c8046e6831533c65f81e0d
jt-96 <jetorrico@outlook.com>
2022-03-24 17:51:56 -0300

    added intro text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,8 +6,8 @@ function Intro() {
     return (
         <div className="row g-0 intro text-center align-items-center justify-content-center" id="home">
             <div className="col">
-                <h1 className="title">Lorem ipsum dolor sit.</h1>
-                <h4 className="subtitle">Lorem ipsum dolor sit amet consectetur.</h4>
+                <h1 className="title">Welcome to my Portfolio.</h1>
+                <h4 className="subtitle">Jonathan Torrico - Full Stack Developer.</h4>
             </div>
         </div>
     )
