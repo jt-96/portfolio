9e517e0f266825b5bb85cab5966ebb024f8fc561
jt-96 <jetorrico@outlook.com>
2022-03-24 15:00:15 -0300

    contact formatting

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,13 +4,13 @@ import './Contact.css';
 function Contact() {
 
     return (
-        <div className="container contact my-3 py-3" id='contact'>
+        <div className="container contact my-3 py-3" id="contact">
             <h2 className="my-2 py-2 text-center">Connect with Me</h2>
             <div className="my-3 py-3 row justify-content-center text-center">
-                <span className='col-md-2'>Icon</span>
-                <span className='col-md-2'>Icon</span>
-                <span className='col-md-2'>Icon</span>
-                <span className='col-md-2'>Icon</span>
+                <span className="col-md-2">Icon</span>
+                <span className="col-md-2">Icon</span>
+                <span className="col-md-2">Icon</span>
+                <span className="col-md-2">Icon</span>
             </div>
         </div>
     )
