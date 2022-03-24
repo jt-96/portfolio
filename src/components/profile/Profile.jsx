509cf1822fb7594226858096ee20443d37c88e00
jt-96 <jetorrico@outlook.com>
2022-03-24 19:56:01 -0300

    created card component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -7,7 +7,7 @@ function Profile() {
         <div className="container profile my-3 py-3">
             <div className="anchor" id="profile"></div>
             <h2 className="py-2">Profile</h2>
-            <p className="py-2">I'm based in MERN Stack and this are the tecnologies I used so far.</p>
+            <p className="py-2">I'm based in MERN Stack and this are the tecnologies I used till now.</p>
             <div className="py-3 flex-row text-center">
                 <img src={`${process.env.PUBLIC_URL}/assets/icons/html5.png`}/>
                 <img src={`${process.env.PUBLIC_URL}/assets/icons/css3.png`}/>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import CardProject from '../cardProject/Card';
 import './Projects.css';
 
 function Project() {
@@ -11,43 +11,13 @@ function Project() {
             <p className="py-2">Here are some of my personal projects done so far.</p>
             <div className="py-3 d-flex flex-row justify-content-around">
                 <div>
-                    <Card style={{ width: "18rem" }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
+                    <CardProject />
                 </div>
                 <div>
-                    <Card style={{ width: "18rem" }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
+                    <CardProject />
                 </div>
                 <div>
-                    <Card style={{ width: "18rem" }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                            </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
+                    <CardProject />
                 </div>
             </div>
         </div>
