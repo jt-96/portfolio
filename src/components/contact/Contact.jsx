f4d3f9d21956dc59f7f8cf4e1442dcea4174a064
jt-96 <jetorrico@outlook.com>
2022-03-25 01:24:58 -0300

    added alt attribute to imgs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,16 +10,16 @@ function Contact() {
             <h2 className="my-2 py-2 text-center contactTitle">Connect with Me</h2>
             <div className="flex-row text-center">
                 <a href={cv} target="_blank" rel="noopener noreferrer">
-                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/cv.png`} />
+                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/cv.png`} alt="View Curriculum Vitae" />
                 </a>
                 <a href="mailto:jetorrico@outlook.com" rel="noopener noreferrer">
-                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/email.png`} />
+                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/email.png`} alt="Email Address"/>
                 </a>
                 <a href="https://ar.linkedin.com/in/jonathan-torrico" target="_blank" rel="noopener noreferrer">
-                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/linkedin.png`} />
+                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/linkedin.png`} alt="LinkedIn Profile"/>
                 </a>
                 <a href="https://github.com/jt-96" target="_blank" rel="noopener noreferrer">
-                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/github.png`} />
+                    <img className='my-5 px-2' src={`${process.env.PUBLIC_URL}/assets/contact/github.png`} alt="GitHub Profile"/>
                 </a>
             </div>
         </div>
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -9,13 +9,13 @@ function Profile() {
             <h2 className="py-2">Profile</h2>
             <p className="py-2">I'm based in MERN Stack and this are the tecnologies I used till now.</p>
             <div className="py-3 flex-row text-center">
-                <img src={`${process.env.PUBLIC_URL}/assets/icons/html5.png`}/>
-                <img src={`${process.env.PUBLIC_URL}/assets/icons/css3.png`}/>
-                <img src={`${process.env.PUBLIC_URL}/assets/icons/js.png`}/>
-                <img src={`${process.env.PUBLIC_URL}/assets/icons/react.png`}/>
-                <img src={`${process.env.PUBLIC_URL}/assets/icons/node.png`}/>
-                <img src={`${process.env.PUBLIC_URL}/assets/icons/mongodb.png`}/>
-                <img src={`${process.env.PUBLIC_URL}/assets/icons/git.png`}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/html5.png`} alt="HTML5 Logo"/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/css3.png`} alt="CSS3 Logo"/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/js.png`} alt="JavaScript Logo"/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/react.png`} alt="React.js Logo"/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/node.png`} alt="Node.js Logo"/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/mongodb.png`} alt="MongoDB Logo"/>
+                <img src={`${process.env.PUBLIC_URL}/assets/icons/git.png`} alt="Git Logo"/>
             </div>
 
         </div>
