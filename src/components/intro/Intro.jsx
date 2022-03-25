fc8d57d5a47cc29534aba37b85cd668d7fe5d04e
jt-96 <jetorrico@outlook.com>
2022-03-25 01:14:32 -0300

    moved intro text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,9 +4,9 @@ import './Intro.css';
 function Intro() {
 
     return (
-        <div className="row g-0 intro text-center align-items-center justify-content-center" id="home">
-            <div className="col">
-                <h1 className="title">Welcome to my Portfolio.</h1>
+        <div className="row g-0 intro text-center justify-content-center" id="home">
+            <div className="col mt-5">
+                <h1 className="title mt-5">Welcome to my Portfolio.</h1>
                 <h4 className="subtitle">Jonathan Torrico - Full Stack Developer</h4>
             </div>
         </div>
