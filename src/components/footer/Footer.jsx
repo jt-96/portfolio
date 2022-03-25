01ed5b676508427b8734fd2c88f072cd447000cb
jt-96 <jetorrico@outlook.com>
2022-03-24 21:02:14 -0300

    added footer text

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,10 @@ function Footer() {
     return (
         <div className="footer col text-center py-2">
             <div className="row g-0">
-                <span>Lorem ipsum dolor sit amet consectetur.</span>
-                <span>Lorem ipsum dolor sit amet.</span>
+                <span>Made with Create-React-App - Background by <a href="http://1041uuu.tumblr.com">1041uuu</a></span>
+                <span>
+                    From <img src="https://flagcdn.com/w20/ar.png" srcSet="https://flagcdn.com/w40/ar.png 2x" width="20" alt="Argentina" /> with &#x1F497;	
+                </span>
             </div>
         </div>
     )
