e7f2da72fc8d7fb72b05e5fe3a77de5a97879bcc
jt-96 <jetorrico@outlook.com>
2022-09-23 20:26:48 -0300

    add qr code, adjust position

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,12 +3,19 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div className="footer col text-center py-2">
-            <div className="row g-0">
-                <span>Made with React - Background by <a href="http://1041uuu.tumblr.com">1041uuu</a></span>
-                <span>
-                    From <img src="https://flagcdn.com/w20/ar.png" srcSet="https://flagcdn.com/w40/ar.png 2x" width="20" alt="Argentina" /> with &#x1F497;	
-                </span>
+        <div className="footer row text-center py-2 g-0">
+            <div className="col-1">
+                <a href="http://qr.afip.gob.ar/?qr=POPelgui7DIvzLfNnvhThQ,," target="_F960AFIPInfo">
+                    <img className="qr" src="http://www.afip.gob.ar/images/f960/DATAWEB.jpg" border="0"></img>
+                </a>
+            </div>
+            <div className="col-11">
+                <div className="d-flex flex-column justify-content-center">
+                    <span>Made with React - Background by <a href="http://1041uuu.tumblr.com">1041uuu</a></span>
+                    <span>
+                        From <img src="https://flagcdn.com/w20/ar.png" srcSet="https://flagcdn.com/w40/ar.png 2x" width="20" alt="Argentina" /> with &#x1F497;
+                    </span>
+                </div>
             </div>
         </div>
     )
