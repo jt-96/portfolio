21c96ce584671a37cf716662c0fd026d21b46350
jt-96 <jetorrico@outlook.com>
2024-01-25 17:25:35 -0300

    feature: several style adjustments, include conditional whether links are null or not

--- src/components/cardProject/Card.jsx
+++ src/components/cardProject/Card.jsx
@@ -5,19 +5,38 @@ import './Card.css';
 function CardProject(props) {
 
     return (
-        <Card className="col-md-3 my-3" style={{ width: "18rem" }}>
+        <Card className="col-md-3 my-3" style={{ width: "20rem" }}>
             <Card.Img variant="top" src={`${process.env.PUBLIC_URL}` + props.project.image} />
-            <Card.Body>
+            <Card.Body className="d-flex flex-column flex-grow-1">
                 <Card.Title>{props.project.name}</Card.Title>
                 <Card.Text>{props.project.description}</Card.Text>
-                <div className="row">
-                    <div className="d-flex justify-content-between">
-                        <Button className="" variant="primary" href={props.project.projectLink} target="_blank" rel="noopener noreferrer">
-                            Visit Page
-                        </Button>
-                        <Button className="" variant="primary" href={props.project.gitLink} target="_blank" rel="noopener noreferrer">
-                            GitHub Repo
-                        </Button>
+                <div className="row d-flex flex-grow-1 align-items-end">
+                    <div className="d-flex justify-content-between flex-grow-1">
+
+                        {
+                            props.project.projectLink ?
+
+                                <Button className="" variant="primary" href={props.project.projectLink} target="_blank" rel="noopener noreferrer">
+                                    Visit Page
+                                </Button>
+
+                                :
+
+                                null
+                        }
+
+                        {
+                            props.project.gitLink ?
+
+                                <Button className="" variant="primary" href={props.project.gitLink} target="_blank" rel="noopener noreferrer">
+                                    GitHub Repo
+                                </Button>
+
+                                :
+
+                                null
+                        }
+
                     </div>
                 </div>
             </Card.Body>
