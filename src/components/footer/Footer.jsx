29ecfd482b7d1ec11c63b34c44fc920c50da9d01
jt-96 <jetorrico@outlook.com>
2024-01-25 17:26:03 -0300

    refactor: hide afip qr

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,12 +4,12 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer row text-center py-2 g-0">
-            <div className="col-1">
+            {/* <div className="col-1">
                 <a href="http://qr.afip.gob.ar/?qr=POPelgui7DIvzLfNnvhThQ,," target="_F960AFIPInfo">
                     <img className="qr" src="http://www.afip.gob.ar/images/f960/DATAWEB.jpg" border="0"></img>
                 </a>
-            </div>
-            <div className="col-11">
+            </div> */}
+            <div className="">
                 <div className="d-flex flex-column justify-content-center">
                     <span>Made with React - Background by <a href="http://1041uuu.tumblr.com">1041uuu</a></span>
                     <span>
