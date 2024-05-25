73b920857c0ca5e09d9976b56a6046bc16bb6883
jt-96 <jetorrico@outlook.com>
2024-05-25 14:42:49 -0300

    feat: redirect to new portfolio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Suspense, lazy } from 'react';
-import './App.css';
+import React, { useEffect, Suspense, lazy } from "react";
+import "./App.css";
 // import Video from './components/video/Video';
-import NavBar from './components/navBar/NavBar';
-import Intro from './components/intro/Intro';
-import About from './components/about/About';
-import Profile from './components/profile/Profile';
-import Project from './components/projects/Projects';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+import NavBar from "./components/navBar/NavBar";
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+import Profile from "./components/profile/Profile";
+import Project from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 function App() {
+  useEffect(() => {
+    window.location.href = "https://jt-96.github.io/portfolio-v2/";
+  }, []);
 
-  const Video = lazy(() => import('./components/video/Video'))
+  const Video = lazy(() => import("./components/video/Video"));
 
   return (
-    <Suspense fallback={<div className='d-flex justify-content-center align-items-center vh-100'>Loading...</div>}>
-
+    <Suspense
+      fallback={
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          Loading...
+        </div>
+      }
+    >
       <div className="App">
         <Video />
         <Intro />
@@ -25,7 +33,7 @@ function App() {
         <Project />
         <Contact />
         <Footer />
-      </div >
+      </div>
     </Suspense>
   );
 }
