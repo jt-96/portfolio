38efa88c52cc27aeffe2f714fd26e306a882016e
jt-96 <jetorrico@outlook.com>
2024-05-25 14:48:43 -0300

    Updates

--- static/js/89.b9eb3c24.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[89],{89:(o,e,t)=>{t.r(e),t.d(e,{default:()=>r});t(43);var s=t(579);const r=function(){return(0,s.jsx)("video",{id:"bg-video",autoPlay:!0,loop:!0,muted:!0,children:(0,s.jsx)("source",{src:"".concat("/portfolio","/assets/video/background.mp4"),type:"video/mp4"})})}}}]);
-//# sourceMappingURL=89.b9eb3c24.chunk.js.map
